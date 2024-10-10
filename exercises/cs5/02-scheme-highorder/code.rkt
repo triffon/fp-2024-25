77d70bf96239eb2f8ac7de520ece6692b7c7f276
dexterlb <dexterlb@qtrp.org>
2024-10-10 17:13:43 +0300

    accumulate-n

--- exercises/cs5/02-scheme-highorder/code.rkt
+++ exercises/cs5/02-scheme-highorder/code.rkt
@@ -39,3 +39,43 @@
 
 (define (fact x)          (accumulate-interval * 1 1 x))
 (define fact2 (lambda (x) (accumulate-interval * 1 1 x)))
+
+(define (square x) (* x x))
+(define (double x) (* 2 x))
+
+(define (o f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated f n x)
+  (if (= n 0)
+      x
+      (repeated f (- n 1) (f x))))
+
+(define (repeat1 f n)
+  (lambda (x) (repeated f n x)))
+
+(define (repeat2 f n)
+  (if (= n 0)
+      (lambda (x) x)
+      (lambda (x) ((o (repeat2 f (- n 1)) f) x))))
+
+(define (repeat3 f n)
+  (if (= n 0)
+      (lambda (x) x)
+      ((o (repeat3 f (- n 1)) f))))
+
+(define (repeat4 f n)
+  (repeated (lambda (g) (o f g)) n (lambda (x) x)))
+
+(define (accumulate-n op init f begin end)
+  (if (> begin end)
+      init
+      (op (f begin) (accumulate-n op init f (+ 1 begin) end))))
+
+(define (count p a b)
+  (accumulate-n
+   +
+   0
+   (lambda (x) (if (p x) 1 0))
+   a b))
+   
