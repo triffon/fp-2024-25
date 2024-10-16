73867fc41daf4d2cd869199761d05bf9934f338e
Alexander Ivanov <61185321+Alexxx2207@users.noreply.github.com>
2024-10-16 13:05:49 +0300

    Добавяне на итеративно бързо степенуване

---
+++ lectures/scheme/iterative_quick_pow.scm
@@ -0,0 +1,17 @@
+(define (fast-expt base exponent)
+    (define (square x) (* x x))
+    (define (for b e product)
+        (cond 
+            ((= e 0) 
+                (if (< exponent 0) (/ 1 product) product)
+            )
+            ((even? e)
+                (for (square b) (/ e 2) product)
+            )
+            (else 
+                (for b (- e 1) (* product b))
+            )
+        )
+    )
+    (for base (abs exponent) 1)
+)
