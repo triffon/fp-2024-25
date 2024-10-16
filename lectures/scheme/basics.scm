a26cc42628f2335d8b2bd33de8271dcc4dac3375
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2024-10-16 10:02:46 +0300

    Примери за рекурсивни и итеративни процеси

--- lectures/scheme/basics.scm
+++ lectures/scheme/basics.scm
@@ -8,3 +8,22 @@
   (define (fact n) (* n 10))
   (if (= n 0) 1
       (* n (fact (- n 1)))))
+
+(define (pow x n)
+  (cond ((= n 0) 1)
+        ((< n 0) (/ 1 (pow x (- n))))
+        (else (* x (pow x (- n 1))))))
+
+(define (qpow x n)
+  (define (sq x) (* x x))
+  (cond ((= n 0) 1)
+        ((< n 0) (/ 1 (qpow x (- n))))
+        ((even? n) (sq (qpow x (quotient n 2))))
+        (else (* x (qpow x (- n 1))))))
+
+;; (qpow 2 2) --> (qpow (qpow 2 1) 2) --> (qpow 2 2) --> ....
+
+
+(define (fib n)
+  (if (<= n 1) n
+      (+ (fib (- n 1)) (fib (- n 2)))))
