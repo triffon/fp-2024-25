7cf307b426fb9d255d3482afbdea15f73d627356
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2024-10-16 10:11:16 +0300

    Итеративна реализация на числа на Фибоначи

--- lectures/scheme/basics.scm
+++ lectures/scheme/basics.scm
@@ -27,3 +27,10 @@
 (define (fib n)
   (if (<= n 1) n
       (+ (fib (- n 1)) (fib (- n 2)))))
+
+(define (fib n)
+  (define (iter i fi fi-1)
+    (if (= i n) fi
+        (iter (+ i 1) (+ fi fi-1) fi)))
+  (if (= n 0) 0
+      (iter 1 1 0)))
