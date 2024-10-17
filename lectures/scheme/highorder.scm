d8a78e70613c0fcd56c6d5f4d1a39761ee6cf325
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2024-10-17 09:55:10 +0300

    Примери за реализация на функции от по-висок ред

---
+++ lectures/scheme/highorder.scm
@@ -0,0 +1,25 @@
+(define fact (lambda (n)
+               (if (= n 0) 1 (* n (fact (- n 1))))))
+
+(define (accumulate op nv a b term next)
+  (if (> a b) nv
+      (op (term a) (accumulate op nv (next a) b term next))))
+
+(define (id x) x)
+(define (1+ x) (+ x 1))
+
+(define (fact n)
+  (accumulate * 1 1 n (lambda (i) i) 1+))
+
+(define (pow x n)
+  (accumulate * 1 1 n (lambda (i) x) 1+))
+
+(define (myexp x n)
+  (accumulate + 0 0 n (lambda (i) (/ (pow x i) (fact i))) 1+))
+
+(define (myexp x n)
+  (accumulate + 0 0 n (lambda (i) (/ (accumulate * 1 1 i (lambda (i) x) 1+)
+                                     (accumulate * 1 1 i (lambda (i) i) 1+))) 1+))
+
+(define (exists? a b p?)
+  (accumulate (lambda (x y) (or x y)) #f a b p? 1+))
