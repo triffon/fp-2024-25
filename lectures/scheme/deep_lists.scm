6679ee422263a5b37b8eb15dfde9c7aee89131be
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2024-11-06 09:54:01 +0200

    Функции за работа с дълбоки списъци

--- lectures/scheme/deep_lists.scm
+++ lectures/scheme/deep_lists.scm
@@ -6,3 +6,39 @@
   (cond ((null? dl) 0)
         ((atom? dl) 1)
         (else (+ (count-atoms (car dl)) (count-atoms (cdr dl))))))
+
+(define (count-atoms dl)
+  (deep-foldr 0 (lambda (x) 1) + dl))
+
+(define (flatten dl)
+  (cond ((null? dl) '())
+        ((atom? dl) (list dl))
+        (else (append (flatten (car dl)) (flatten (cdr dl))))))
+
+(define (flatten dl)
+  (deep-foldr '() list append dl))
+
+(define (snoc x l) (append l (list x)))
+
+(define (deep-reverse dl)
+  (cond ((null? dl) '())
+        ((atom? dl) dl)
+        (else (snoc (deep-reverse (car dl)) (deep-reverse (cdr dl))))))
+
+(define (id x) x)
+
+(define (deep-reverse dl)
+  (deep-foldr '() id snoc dl))
+
+(define (deep-foldr nv term op dl)
+  (cond ((null? dl) nv)
+        ((atom? dl) (term dl))
+        (else (op (deep-foldr nv term op (car dl)) (deep-foldr nv term op (cdr dl))))))
+
+(define (foldr op nv l)
+  (if (null? l) nv
+      (op (car l) (foldr op nv (cdr l)))))
+
+(define (deep-foldr nv term op dl)
+  (foldr op nv (map (lambda (x) (if (atom? x) (term x) (deep-foldr nv term op x))) dl)))
+
