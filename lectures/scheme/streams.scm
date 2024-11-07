0521749247c60fbe99d03cb26732b0f25152eed7
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2024-11-07 18:07:26 +0200

    Функции за работа с потоци

--- lectures/scheme/streams.scm
+++ lectures/scheme/streams.scm
@@ -11,6 +11,9 @@
 
 (define (force x) (x))
 
+(define s (cons-stream 1 (cons-stream 2 (cons-stream 3 the-empty-stream))))
+(define s2 (cons-stream 3 (cons-stream b the-empty-stream)))
+
 (define (enum a b)
   (if (> a b) the-empty-stream
       (cons-stream a (enum (+ a 1) b))))
@@ -28,7 +31,39 @@
         ((p? (head s)) s)
         (else (find-stream p? (tail s)))))
 
+(define (from n)
+  (cons-stream n (from (+ n 1))))
 
+(define nats (from 0))
 
-(define s (cons-stream 1 (cons-stream 2 (cons-stream 3 the-empty-stream))))
-(define s2 (cons-stream 3 (cons-stream b the-empty-stream)))
+(define (generate-fibs a b)
+  (cons-stream a (generate-fibs b (+ a b))))
+
+(define fibs (generate-fibs 0 1))
+
+(define (map-stream f s)
+  (cons-stream (f (head s)) (map-stream f (tail s))))
+
+(define (filter-stream p? s)
+  (if (p? (head s)) (cons-stream (head s) (filter-stream p? (tail s)))
+                    (filter-stream p? (tail s))))
+
+(define (zip-streams op s1 s2)
+  (cons-stream (op (head s1) (head s2))
+               (zip-streams op (tail s1) (tail s2))))
+
+(define (1+ x) (+ x 1))
+
+(define ones (cons-stream 1 ones))
+(define nats (cons-stream 0 (zip-streams + ones nats)))
+(define nats (cons-stream 0 (map-stream 1+ nats)))
+(define fibs
+  (cons-stream 0 (cons-stream 1 (zip-streams + fibs (tail fibs)))))
+
+(define (nondivisor d) (lambda (n) (> (remainder n d) 0)))
+
+(define (sieve stream)
+  (cons-stream (head stream) (sieve (filter-stream (nondivisor (head stream)) (tail stream)))))
+
+(define primes (sieve (from 2)))
+
