73e897b5f197ca651e7cc50e161b092cd080d0fa
Atanas Ormanov <atanasormanow@gmail.com>
2025-01-13 14:30:17 +0200

    Remove ghc options

--- exercises/cs6/11/solutions.hs
+++ exercises/cs6/11/solutions.hs
@@ -1,14 +1,3 @@
--- cover all cases!
-{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
--- warn about incomplete patterns v2
-{-# OPTIONS_GHC -fwarn-incomplete-uni-patterns #-}
--- write all your toplevel signatures!
-{-# OPTIONS_GHC -fwarn-missing-signatures #-}
--- use different names!
-{-# OPTIONS_GHC -fwarn-name-shadowing #-}
--- use all your pattern matches!
-{-# OPTIONS_GHC -fwarn-unused-matches #-}
-
 import Prelude hiding (lookup)
 
 g :: (Eq a, Num a) => [(a, [a])]
